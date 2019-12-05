a855b5bdf71979159c582dee7bae04ecde61aeed
David <525065478@qq.com>
2019-12-06 02:09:58 +0800

    修改自动构建文件

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ mv ./docs-dist ../
 
 
 git checkout --orphan gh-pages
-git rm -rf .
 rm \'.gitignore\'
 rm \'.vscode\'
+rm -rf ./*
 
 mv ../docs-dist ./
 mv ./docs-dist/* ./
@@ -14,3 +14,8 @@ rm -rf ./docs-dist
 
 git add .
 git commit -m 'Blog项目构建时间：'$(date "+%Y%m%d-%H:%M:%S")
+
+git push origin gh-pages -f
+
+git checkout master
+git branch gh-pages -D
