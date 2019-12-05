d48a3c65c6d354c05885384447c41bbbede7c927
David <525065478@qq.com>
2019-12-06 02:21:24 +0800

    [修改ci文件]

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,14 @@ rm \'.gitignore\'
 rm \'.vscode\'
 rm -rf ./*
 
+rm -rf ./docs-dist
 mv ../docs-dist ./
 mv ./docs-dist/* ./
-rm -rf ./docs-dist
 
 git add .
 git commit -m 'Blog项目构建时间：'$(date "+%Y%m%d-%H:%M:%S")
 
-git push origin gh-pages -f
+# git push origin gh-pages -f
 
-git checkout master
-git branch gh-pages -D
+# git checkout master
+#git branch gh-pages -D
