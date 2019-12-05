ff68e6a94ba0006c992cd04d1695a48305703d5f
David <525065478@qq.com>
2019-12-06 02:02:42 +0800

    [修改] 修改deploy.sh文件

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,16 @@
-#!/usr/bin/env sh
+yarn docs:build
 
-# 确保脚本抛出遇到的错误
-set -e
+mv ./docs-dist ../
 
-# 生成静态文件
-npm run build
 
-# 进入生成的文件夹
-cd docs-dist
+git checkout --orphan gh-pages
+git rm -rf .
+rm \'.gitignore\'
+rm \'.vscode\'
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+mv ../docs-dist ./
+mv ./docs-dist/* ./
+rm -rf ./docs-dist
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:wenwenwei/blog.git master:gh-pages
-
-cd -
+git add .
+git commit -m 'Blog项目构建时间：'$(date "+%Y%m%d-%H:%M:%S")
